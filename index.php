cdb2a96bb345a02ac588205d99d2d4c53231e8b4
VittorioBarella <vittorio.br@hotmail.com>
2018-11-21 10:07:50 -0200

    Alteração no index.php e Criação dos formulários para cadastro de autores e livros

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
       <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
       <meta name="description" content="">
       <meta name="author" content="">
-      <title>Biblioteca HUE </title>
+      <title>Página Inicial</title>
       <!-- Bootstrap core CSS -->
       <link href="bootstrap/css/bootstrap.min.css" rel="stylesheet">
       <!-- Custom styles for this template -->
@@ -13,131 +13,95 @@
    </head>
    <body>
       <div id="wrapper">
-         <!-- Sidebar -->
-         <div id="sidebar-wrapper">
-            <ul class="sidebar-nav">
-               <li class="sidebar-brand">
-                  <a href="index.php">
-                  Página Inicial
-                  </a>
-               </li>
-               <li>
-                  <a href="#">Cadastro de Autores</a>
-               </li>
-               <li>
-                  <a href="#">Cadastro de Livros</a>
-               </li>
-               <li>
-                  <a href="#">Sobre</a>
-               </li>
-               <li>
-                  <a href="#">Contato</a>
-               </li>
-            </ul>
-         </div>
-         <!-- /#sidebar-wrapper -->
+      <!-- Sidebar -->
+      <div id="sidebar-wrapper">
+         <ul class="sidebar-nav">
+            <li class="sidebar-brand">
+               <a href="index.php">
+               Página Inicial
+               </a>
+            </li>
+            <li>
+               <a href="form_cadastraAutores.php">Cadastro de Autores</a>
+            </li>
+            <li>
+               <a href="form_cadastraLivros.php">Cadastro de Livros</a>
+            </li>
+         </ul>
+      </div>
+      <!-- /#sidebar-wrapper -->
+      <!-- Page Content -->
+      <div id="page-content-wrapper">
+      <div class="container-fluid">
+      <body>
          <!-- Page Content -->
-         <div id="page-content-wrapper">
-            <div class="container-fluid">
-               <body>
-                  <!-- Page Content -->
-                  <div class="container">
-                     <!-- Page Heading -->
-                      <h1 class="tituloBoasVindas">Bem-Vindo !</h1>
-                      <div>
-                         <a href="#menu-toggle" class="btn btn-secondary" id="menu-toggle">Menu</a>
-                     </div>
-                      
-                     <h2 class="my-4">Page Heading
-                        <small>Secondary Text</small>
-                     </h2>
-                   
-                     <div class="row">
-                        <div class="col-lg-6 portfolio-item">
-                           <div class="card h-100">
-                              <a href="#"><img class="card-img-top" src="http://placehold.it/700x400" alt=""></a>
-                              <div class="card-body">
-                                 <h4 class="card-title">
-                                    <a href="#">Project One</a>
-                                 </h4>
-                                 <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam viverra euismod odio, gravida pellentesque urna varius vitae.</p>
-                              </div>
-                           </div>
-                        </div>
-                        <div class="col-lg-6 portfolio-item">
-                           <div class="card h-100">
-                              <a href="#"><img class="card-img-top" src="http://placehold.it/700x400" alt=""></a>
-                              <div class="card-body">
-                                 <h4 class="card-title">
-                                    <a href="#">Project Two</a>
-                                 </h4>
-                                 <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fugit aliquam aperiam nulla perferendis dolor nobis numquam, rem expedita, aliquid optio, alias illum eaque. Non magni, voluptates quae, necessitatibus unde temporibus.</p>
-                              </div>
-                           </div>
-                        </div>
-                        <div class="col-lg-6 portfolio-item">
-                           <div class="card h-100">
-                              <a href="#"><img class="card-img-top" src="http://placehold.it/700x400" alt=""></a>
-                              <div class="card-body">
-                                 <h4 class="card-title">
-                                    <a href="#">Project Three</a>
-                                 </h4>
-                                 <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam viverra euismod odio, gravida pellentesque urna varius vitae.</p>
-                              </div>
-                           </div>
-                        </div>
-                        <div class="col-lg-6 portfolio-item">
-                           <div class="card h-100">
-                              <a href="#"><img class="card-img-top" src="http://placehold.it/700x400" alt=""></a>
-                              <div class="card-body">
-                                 <h4 class="card-title">
-                                    <a href="#">Project Four</a>
-                                 </h4>
-                                 <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fugit aliquam aperiam nulla perferendis dolor nobis numquam, rem expedita, aliquid optio, alias illum eaque. Non magni, voluptates quae, necessitatibus unde temporibus.</p>
-                              </div>
-                           </div>
-                        </div>
-                        <div class="col-lg-6 portfolio-item">
-                           <div class="card h-100">
-                              <a href="#"><img class="card-img-top" src="http://placehold.it/700x400" alt=""></a>
-                              <div class="card-body">
-                                 <h4 class="card-title">
-                                    <a href="#">Project Five</a>
-                                 </h4>
-                                 <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam viverra euismod odio, gravida pellentesque urna varius vitae.</p>
-                              </div>
-                           </div>
-                        </div>
-                        <div class="col-lg-6 portfolio-item">
-                           <div class="card h-100">
-                              <a href="#"><img class="card-img-top" src="http://placehold.it/700x400" alt=""></a>
-                              <div class="card-body">
-                                 <h4 class="card-title">
-                                    <a href="#">Project Six</a>
-                                 </h4>
-                                 <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fugit aliquam aperiam nulla perferendis dolor nobis numquam, rem expedita, aliquid optio, alias illum eaque. Non magni, voluptates quae, necessitatibus unde temporibus.</p>
-                              </div>
-                           </div>
-                        </div>
-                     </div>
-                     <!-- /.row -->
-                  </div>
-                  <!-- /.container -->      
-   </body>
-   </div>
-   </div>
-   <!-- /#page-content-wrapper -->
-   </div>
-   <!-- /#wrapper -->
-   <!-- Bootstrap core JavaScript -->
-   <script src="jquery/jquery.min.js"></script>
-   <script src="bootstrap/js/bootstrap.bundle.min.js"></script>
-   <!-- Menu Toggle Script -->
-   <script>
-      $("#menu-toggle").click(function(e) {
-          e.preventDefault();
-          $("#wrapper").toggleClass("toggled");
-      });
-   </script>
+         <div class="container">
+         <!-- Page Heading -->
+         <h1 class="tituloBoasVindas">Bem-Vindo !</h1>
+         <div>
+            <a href="#menu-toggle" class="btn btn-secondary btn-menu" id="menu-toggle">Menu</a>
+         </div>
+         <h2> Tabela </h2>
+         <div class="tabela">
+            <table class="table table-striped table-bordered">
+               <thead>
+                  <tr>
+                     <th scope="col">#</th>
+                     <th scope="col">First</th>
+                     <th scope="col">Last</th>
+                     <th scope="col">Handle</th>
+                  </tr>
+               </thead>
+               <tbody>
+                  <tr>
+                     <th scope="row">1</th>
+                     <td>Mark</td>
+                     <td>Otto</td>
+                     <td>@mdo</td>
+                  </tr>
+                  <tr>
+                     <th scope="row">2</th>
+                     <td>Jacob</td>
+                     <td>Thornton</td>
+                     <td>@fat</td>
+                  </tr>
+                  <tr>
+                     <th scope="row">3</th>
+                     <td>Larry</td>
+                     <td>the Bird</td>
+                     <td>@twitter</td>
+                  </tr>
+                  <tr>
+                     <th scope="row">4</th>
+                     <td>Mark</td>
+                     <td>Otto</td>
+                     <td>@mdo</td>
+                  </tr>
+                  <tr>
+                     <th scope="row">5</th>
+                     <td>Jacob</td>
+                     <td>Thornton</td>
+                     <td>@fat</td>
+                  </tr>
+                  <tr>
+                     <th scope="row">6</th>
+                     <td>Larry</td>
+                     <td>the Bird</td>
+                     <td>@twitter</td>
+                  </tr>
+               </tbody>
+            </table>
+         </div>
+         <!-- /#wrapper -->
+         <!-- Bootstrap core JavaScript -->
+         <script src="jquery/jquery.min.js"></script>
+         <script src="bootstrap/js/bootstrap.bundle.min.js"></script>
+         <!-- Menu Toggle Script -->
+         <script>
+            $("#menu-toggle").click(function(e) {
+                e.preventDefault();
+                $("#wrapper").toggleClass("toggled");
+            });
+         </script>
    </body>
 </html>
